9f3ad032f4355a7f226687d1fa00a73badc0d4e4
surya <surya@DESKTOP-FHBRPJ7>
2025-03-03 23:26:55 -0500

    Initial Commit

---
+++ SuryaSolution24/PostBackDemo/Controllers/PostBackDemoController.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Intrinsics.X86;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostBackDemo.Controllers
+{
+    public class PostBackDemoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.msg = "Hello,Welcome";
+            return View();
+        }
+        [HttpPost]  
+        public IActionResult Index(string id)
+        {
+            ViewBag.msg = "Hi, Welcome Back";
+            return View();
+        }
+
+        
+    }
+}
